2e3561596cf91f18d0372984d56524f068fd30f1
olaldeJP <olaldejp@gmail.com>
2024-04-18 18:33:30 -0300

    update

--- app/app.js
+++ app/app.js
@@ -23,30 +23,6 @@ export class Server {
     this.#server.use("/api", apiRouter);
     this.#server.use("/", webRouter);
     this.#server.use(errorManager);
-    /* 
-TEST CON SWAGGER:
-    import swaggerUiExpress from "swagger-ui-express";
-    import swaggerJSDoc from "swagger-jsdoc";
-    const SWAGGER_CONFIG = {
-      definition: {
-        openapi: "3.0.1",
-        info: {
-          version: "1",
-          title: "Swagger ",
-          description: "Swagger For Ecommers",
-        },
-      },*/
-    //apis: ["./docs/**/*.yaml"]
-    /*};
-
-    const spec = swaggerJSDoc(SWAGGER_CONFIG);
-
-    this.app.use(
-      "/api-doc",
-      swaggerUiExpress.serve,
-      swaggerUiExpress.setup(spec)
-    );
-   */
     logger.INFO(`Server config - Success`);
   }
 
