0b43db18058cf4cd70b12c6ecba3f4e34043a692
olaldeJP <olaldejp@gmail.com>
2024-04-23 11:43:59 -0300

    test

--- src/test/test.js
+++ src/test/test.js
@@ -1,4 +1,13 @@
+import { Server } from "../../app/app.js";
+import { PORT, URL_MONGO } from "../config/config.js";
+import Assert from "assert";
+import { usersService } from "../services/users.Service.js";
+const assert = Assert.strict;
 describe("System Test:", function () {
+  before(function () {
+    this.server = new Server(URL_MONGO);
+    this.server.connect(PORT);
+  });
   describe("Unit Test", function () {
     describe("UserServices ", function () {
       describe("Register", function () {
